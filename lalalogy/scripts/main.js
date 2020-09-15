c1cf59041903385cbd33cdbe1cf2203eaf72e8cc
supernovalx <hoanganhgo9@gmail.com>
2020-09-15 17:33:11 +0700

    lalalogy

---
+++ lalalogy/scripts/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('nav');
+    let windowPosition = window.scrollY > 28;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
