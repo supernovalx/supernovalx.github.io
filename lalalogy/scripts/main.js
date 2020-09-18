3b9187c457ace8b327ba7d0e84254a3006dba871
supernovalx <hoanganhgo9@gmail.com>
2020-09-18 20:00:35 +0700

    add chatbot page

--- lalalogy/scripts/main.js
+++ lalalogy/scripts/main.js
@@ -1,9 +1,3 @@
-window.addEventListener("scroll", function () {
-    let header = document.querySelector("nav");
-    let windowPosition = window.scrollY > 28;
-    header.classList.toggle("scrolling-active", windowPosition);
-});
-
 var chatbotAnim = null;
 document
     .querySelector("#robo-head")
