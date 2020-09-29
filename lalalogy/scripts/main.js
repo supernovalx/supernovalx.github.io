d20c6c3cab7fda47a127f0fa1dc6529c46269585
supernovalx <hoanganhgo9@gmail.com>
2020-09-29 21:33:29 +0700

    animation

--- lalalogy/scripts/main.js
+++ lalalogy/scripts/main.js
@@ -67,6 +67,63 @@ gsap.utils.toArray(".bgelement").forEach(star => {
       onCompleteParams: [target, prop, min, max]
     });
   
-  }
-  
-  
+}
+
+var lineDuration = .15;
+var lineEase = "back"
+var cardChatbotAnimation = gsap.timeline({repeat: -1, paused: true})
+.from("#chatbotcard-line1", {opacity: 0, scaleX: 0, duration: lineDuration, ease: lineEase})
+.from("#chatbotcard-line2", {opacity: 0, scaleX: 0, duration: lineDuration, ease: lineEase})
+.from("#chatbotcard-line3", {opacity: 0, scaleX: 0, duration: lineDuration, ease: lineEase})
+.from("#chatbotcard-line4", {opacity: 0, scaleX: 0, x: 0, duration: lineDuration, ease: lineEase}, "+=.2")
+.from("#chatbotcard-line5", {opacity: 0, scaleX: 0, x: 0, duration: lineDuration, ease: lineEase})
+.from("#chatbotcard-line6", {opacity: 0, scaleX: 0, x: 0, duration: lineDuration, ease: lineEase});
+
+function cardChatbotMouseEnter() {
+    cardChatbotAnimation.play();
+}
+
+function cardChatbotMouseLeave() {
+    cardChatbotAnimation.seek(0);
+    cardChatbotAnimation.pause();
+}
+
+var popupDuration = .5;
+var popupEase = "power1.in"
+var popupEaseOut = "power1.out"
+var cardMobileAnimation = gsap.timeline({repeat: -1, paused: true})
+.from("#mobilecard-popup1", {opacity: 0.2, scale: 0, x:159, y: 80, duration: popupDuration, ease: popupEase})
+.from("#mobilecard-popup2", {opacity: 0.2, scale: 0, x: 60, y:120, duration: popupDuration, ease: popupEase}, "-=.2")
+.from("#mobilecard-popup3", {opacity: 0.2, scale: 0, duration: popupDuration, ease: popupEase}, "-=.2")
+.from("#mobilecard-popup4", {opacity: 0.2, scale: 0, x: 60, y: 30, duration: popupDuration, ease: popupEase}, "-=.2")
+.to("#mobilecard-popup1", {opacity: 0, scale: 0, duration: popupDuration, ease: popupEaseOut})
+.to("#mobilecard-popup2", {opacity: 0, scale: 0, duration: popupDuration, ease: popupEaseOut}, "-=.2")
+.to("#mobilecard-popup3", {opacity: 0, scale: 0, duration: popupDuration, ease: popupEaseOut}, "-=.2")
+.to("#mobilecard-popup4", {opacity: 0, scale: 0, duration: popupDuration, ease: popupEaseOut}, "-=.3");
+
+function cardMobileMouseEnter() {
+    cardMobileAnimation.play();
+}
+
+function cardMobileMouseLeave() {
+    cardMobileAnimation.seek(0);
+    cardMobileAnimation.pause();
+}
+
+var gameDuration = .4;
+var gameEase = "none";
+var gameEaseOut = "bounce";
+var cardGameAnimation = gsap.timeline({repeat: -1, paused: true})
+.to("#gamecard-gameboy", {y: 0, duration: gameDuration, ease: gameEase})
+.to("#gamecard-controller", {y: 30, duration: gameDuration, ease: gameEase})
+.to("#gamecard-gameboy", {y: 24, duration: gameDuration, ease: gameEaseOut}, "-=.3")
+.to("#gamecard-controller", {y: 75, duration: gameDuration, ease: gameEaseOut});
+
+function cardGameMouseEnter() {
+    cardGameAnimation.play();
+}
+
+function cardGameMouseLeave() {
+    cardGameAnimation.seek(0);
+    cardGameAnimation.pause();
+}
--- lalalogy/scripts/nav.js
+++ lalalogy/scripts/nav.js
@@ -8,11 +8,19 @@ var modal = document.getElementById('nav-modal');
 function onHamburgerClick() {
     modal.classList = "";
     // gsap.from( '#nav-modal', 1, {autoAlpha:0, y: 100, duration: 5} );
-    gsap.from("#nav-modal", {opacity: 0, duration: 0.2});
+    gsap.fromTo("#nav-modal", {opacity: 0}, {opacity: 1, duration: 0.3});
+    gsap.from("#nav-modal a", {y: 100, stagger: {amount: 0.1}});
 
 }
 
 function onNavModalExit() {
-    modal.classList = "hidden";
+    gsap.timeline().to("#nav-modal a", {y: -400, stagger: {amount: 0.1}})
+    .to("#nav-modal a", {y: 0, duration: 0});
+    gsap.fromTo("#nav-modal", {opacity: 1}, {opacity: 0, duration: 0.2, onComplete: navModalExitComplete});
+    
+}
 
+function navModalExitComplete() {
+    console.log(1);
+    modal.classList = "hidden";
 }
