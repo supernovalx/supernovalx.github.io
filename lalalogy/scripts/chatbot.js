1b04b4d45cb50c1b5b9152ba570c7321a700576e
supernovalx <hoanganhgo9@gmail.com>
2020-10-13 20:22:38 +0700

    chatbot page slidein effect

--- lalalogy/scripts/chatbot.js
+++ lalalogy/scripts/chatbot.js
@@ -1,1 +1,10 @@
-gsap.timeline({repeat: -1, repeatDelay: 5}).from(".chat-line", {display: "none", scale: 0, stagger: 2});
+
+$( document ).ready(function() {
+    gsap.timeline({repeat: -1, repeatDelay: 5}).from(".chat-line", {display: "none", scale: 0, stagger: 2});
+    gsap.from("section", {
+        y: 100,
+        opacity: 0,
+        stagger: .5,
+        ease: "power2.in"
+    });
+});
